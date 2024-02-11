271f79a221f69d4fb39b2c2e987a3a06fa7562c6
Hank Hogan <ysh2291939848@outlook.com>
2024-02-12 01:44:31 +0800

    Initial Commit

---
+++ Ehall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ehall
+//
+//  Created by Hank Hogan on 2024/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ehall/EhallApp.swift
@@ -0,0 +1,17 @@
+//
+//  EhallApp.swift
+//  Ehall
+//
+//  Created by Hank Hogan on 2024/2/12.
+//
+
+import SwiftUI
+
+@main
+struct EhallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
